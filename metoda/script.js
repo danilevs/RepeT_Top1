fee8f0e75c6fabaa28261020450a7460eea961c8
danilevs <44302349+danilevs@users.noreply.github.com>
2021-04-15 15:44:11 +0300

    Update script.js

--- metoda/script.js
+++ metoda/script.js
@@ -1,6 +1,6 @@
 let a = document.getElementById('container');
 
-let count = 21; //колличество картинок/заданий
+let count = 38; //колличество картинок/заданий
 
 count=count+1;
 let linetxt="<br><br>============================<br>";
